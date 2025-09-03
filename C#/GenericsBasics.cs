0c808e6565cfbbd2249ac3a72a221c6f32dbb2a1
Velraaj <velraaj30@gmail.com>
2025-09-03 14:16:23 +0530

    - First check in

---
+++ C#/GenericsBasics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class GenericsBasics
+    {
+        public T Max<T> (T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generics;
+
+public class Program()
+{
+    public static void Main()
+    {
+        var test = new GenericsBasics();
+
+        var biggerInt = test.Max(1, 2);
+        Console.WriteLine(biggerInt);
+
+        var biggerString = test.Max("Velraaj", "ViratKohli");
+        Console.WriteLine(biggerString);
+    }
+}
